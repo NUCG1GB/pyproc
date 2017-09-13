e6db4cf07b2975b73bbf1dbc9fb76a846049735c
Lomanowski, Bart <bart.lomanowski@ccfe.ac.uk>
2017-09-13 16:47:11 +0100

    New repo for pyproc - moving from jet_div_spec

---
+++ pyprocanalyse.py
@@ -0,0 +1,354 @@
+
+# Import external modules
+
+import numpy as np
+# import scipy.io as io
+import os, errno
+import json
+from pyproc import pyproc
+from pyADASutils import adas_adf15_utils, adas_adf11_utils, continuum_utils
+## http://lmfit.github.io/lmfit-py/parameters.html
+from lmfit import minimize, Parameters, report_fit
+
+class PyprocAnalyse(pyproc.Pyproc):
+    """
+        Inherits from Pyproc and adds methods for analysis of synthetic spectra
+    """
+    def __init__(self, input_dict):
+        self.input_dict = input_dict
+
+        tmpstr = input_dict['tranfile'].replace('/','_')
+        tmpstr = tmpstr[3:-5]
+        savedir = input_dict['save_dir'] + tmpstr + '/'
+
+        # Create dir from tran file, if it does not exist
+        try:
+            os.makedirs(savedir)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        self.data2d_save_file = savedir +'pyproc.2ddata.pkl'
+        self.synth_diag_save_file = savedir + 'pyproc.synth_diag.json'
+        self.proc_synth_diag_save_file = savedir+ 'pyproc.proc_synth_diag.json'
+
+        # Read all necessary ADAS data here and store in dict
+        self.spec_line_dict = input_dict['spec_line_dict']
+        self.ADAS_dict = {}
+        Te_rnge = [0.2, 5000]
+        ne_rnge = [1.0e11, 1.0e15]
+        self.ADAS_samples = 100
+        self.ADAS_dict['adf15'] = adas_adf15_utils.get_adas_imp_PECs_interp(self.spec_line_dict,  Te_rnge, ne_rnge, npts=self.ADAS_samples, npts_interp=1000)
+        # Also get adf11 for the ionisation balance fractional abundance. No Te_arr, ne_arr interpolation
+        # available in the adf11 reader at the moment, so generate more coarse array (sloppy!)
+        # TODO: add interpolation capability to the adf11 reader so that adf15 and adf11 are on the same Te, ne grid
+        Te_arr_adf11 = np.logspace(np.log10(Te_rnge[0]), np.log10(Te_rnge[1]), 500)
+        ne_arr_adf11 = np.logspace(np.log10(ne_rnge[0]), np.log10(ne_rnge[1]), 30)
+        self.ADAS_dict['adf11'] = {}
+        for atnum in self.spec_line_dict:
+            if int(atnum) > 1:
+                self.ADAS_dict['adf11'][atnum] = adas_adf11_utils.get_adas_imp_adf11(int(atnum), Te_arr_adf11, ne_arr_adf11)
+            elif int(atnum) == 1:
+                self.ADAS_dict['adf11'][atnum] = adas_adf11_utils.get_adas_H_adf11_interp(Te_rnge, ne_rnge,
+                                                                                          npts=self.ADAS_samples, npts_interp=1000, pwr=True)
+        super().__init__(self.ADAS_dict, tranfile=input_dict['tranfile'],
+                         machine=input_dict['machine'],
+                         interactive_plots = input_dict['interactive_plots'],
+                         spec_line_dict=self.spec_line_dict,
+                         diag_list=input_dict['diag_list'],
+                         calc_synth_spec_features=input_dict['run_options']['calc_synth_spec_features'],
+                         calc_NII_afg_feature=False,
+                         save_synth_diag=True,
+                         synth_diag_save_file=self.synth_diag_save_file,
+                         data2d_save_file=self.data2d_save_file)
+
+        if self.input_dict['run_options']['analyse_synth_spec_features']:
+            # Read synth diag saved data
+            try:
+                with open(self.synth_diag_save_file, 'r') as f:
+                    res_dict = json.load(f)
+                self.analyse_synth_spectra(res_dict)
+            except IOError as e:
+                raise
+
+
+    # Analyse synthetic spectra
+    def analyse_synth_spectra(self, res_dict):
+
+        # Estimate parameters and update res_dict. Call order matters since ne and Te
+        # are needed as constraints
+        #
+        # Electron density estimate from Stark broadening of H6-2 line
+        self.recover_line_int_Stark_ne(res_dict)
+
+        # Electron temperature estimate from ff+fb continuum
+        self.recover_line_int_ff_fb_Te(res_dict)
+
+        # Recombination and Ionization
+        self.recover_line_int_particle_bal(res_dict)
+
+        # delL * neutral density from Ly-alpha assuming excitation dominated
+        self.recover_delL_atomden_product(res_dict)
+
+        if self.proc_synth_diag_save_file:
+            with open(self.proc_synth_diag_save_file, mode='w', encoding='utf-8') as f:
+                json.dump(res_dict, f, indent=2)
+
+    @staticmethod
+    def find_nearest(array, value):
+        idx = (np.abs(array - value)).argmin()
+        return idx, array[idx]
+
+    @staticmethod
+    def residual_lorentz_52(params, x, data=None, eps_data=None):
+
+        cwl = params['cwl'].value
+        area = params['area'].value
+        stark_fwhm = params['stark_fwhm'].value
+
+        model = 1. / (np.power(np.abs(x - cwl), 5. / 2.) + np.power(stark_fwhm / 2.0, 5. / 2.))
+
+        model_area = np.trapz(model, x=x)
+        amp_scal = area / model_area
+        model *= amp_scal
+
+        if data is None:
+            return model
+        if eps_data is None:
+            return (model - data)
+        return (model - data) / eps_data
+
+    @staticmethod
+    def residual_continuo(params, wave, data=None, eps_data=None):
+        delL = params['delL'].value
+        te = params['te_360_400'].value
+        ne = params['ne'].value
+
+        model_ff, model_ff_fb = continuum_utils.adas_continuo_py(wave, te, 1, 1)
+        model_ff = model_ff * ne * ne * delL
+        model_ff_fb = model_ff_fb * ne * ne * delL
+
+        if data is None:
+            return model_ff_fb
+        if eps_data is None:
+            return (model_ff_fb - data)
+        return (model_ff_fb - data) / eps_data
+
+    def recover_line_int_ff_fb_Te(self, res_dict):
+        """
+            RECOVER LINE-AVERAGED ELECTRON TEMPERATURE FROM FF-FB CONTINUUM SPECTRA
+            Balmer edge ratio estimate: 360 nm and 400 nm
+            Balmer ff-fb below edge ratio: 300 nm and 400 nm
+            Balmer ff-fb above edge ratio: 400 nm and 500 nm
+        """
+        cont_ratio_360_400 = continuum_utils.get_fffb_intensity_ratio_fn_T(360.0, 400.0, 1.0, save_output=False, restore=True)
+        cont_ratio_300_360 = continuum_utils.get_fffb_intensity_ratio_fn_T(300.0, 360.0, 1.0, save_output=False, restore=True)
+        cont_ratio_400_500 = continuum_utils.get_fffb_intensity_ratio_fn_T(400.0, 500.0, 1.0, save_output=False, restore=True)
+
+        for diag_key in res_dict.keys():
+            for chord_key in res_dict[diag_key].keys():
+
+                print('Fitting ff+fb continuum spectra, LOS id= :', diag_key, ' ', chord_key)
+
+                wave_fffb = np.asarray(res_dict[diag_key][chord_key]['los_int']['ff_fb_continuum']['wave'])
+                synth_data_fffb = np.asarray(
+                    res_dict[diag_key][chord_key]['los_int']['ff_fb_continuum']['intensity'])
+                idx_300, val = self.find_nearest(wave_fffb, 300.0)
+                idx_360, val = self.find_nearest(wave_fffb, 360.0)
+                idx_400, val = self.find_nearest(wave_fffb, 400.0)
+                idx_500, val = self.find_nearest(wave_fffb, 500.0)
+                ratio_360_400 = synth_data_fffb[idx_360] / synth_data_fffb[idx_400]
+                ratio_300_360 = synth_data_fffb[idx_300] / synth_data_fffb[idx_360]
+                ratio_400_500 = synth_data_fffb[idx_400] / synth_data_fffb[idx_500]
+                icont_ratio, vcont_ratio = self.find_nearest(cont_ratio_360_400[:, 1], ratio_360_400)
+                fit_te_360_400 = cont_ratio_360_400[icont_ratio, 0]
+                icont_ratio, vcont_ratio = self.find_nearest(cont_ratio_300_360[:, 1], ratio_300_360)
+                fit_te_300_360 = cont_ratio_300_360[icont_ratio, 0]
+                icont_ratio, vcont_ratio = self.find_nearest(cont_ratio_400_500[:, 1], ratio_400_500)
+                fit_te_400_500 = cont_ratio_400_500[icont_ratio, 0]
+
+                ##### Add fit Te result to dictionary
+                res_dict[diag_key][chord_key]['los_int']['ff_fb_continuum'] = {
+                    'fit': {'fit_te_360_400': fit_te_360_400, 'fit_te_300_360': fit_te_300_360,
+                            'fit_te_400_500': fit_te_400_500, 'units': 'eV'}}
+
+                ###############################################################
+                # CALCULATE EFFECTIVE DEL_L USING LINE-INT ne AND Te VALUES AND CONTINUUM
+                ###############################################################
+                if res_dict[diag_key][chord_key]['los_int']['stark']['fit']['ne']:
+                    params = Parameters()
+                    params.add('delL', value=0.5, min=0.0001, max=10.0)
+                    params.add('te_360_400', value=fit_te_360_400)
+                    fit_ne = res_dict[diag_key][chord_key]['los_int']['stark']['fit']['ne']
+                    params.add('ne', value=fit_ne)
+                    params['te_360_400'].vary = False
+                    params['ne'].vary = False
+
+                    fit_result = minimize(self.residual_continuo, params, args=(wave_fffb, synth_data_fffb),
+                                          method='leastsq')
+                    data_fit_ff_fb = self.residual_continuo(params, wave_fffb, None, None)
+                    print("Chi squred: ", fit_result.chisqr)
+                    report_fit(params)
+
+                    ##### Add fit delL result to dictionary
+                    res_dict[diag_key][chord_key]['los_int']['ff_fb_continuum']['fit']['delL_360_400'] = \
+                        params['delL'].value
+
+    def recover_line_int_Stark_ne(self, res_dict):
+        """
+            RECOVER LINE-AVERAGED ELECTRON DENSITY FROM H6-2 STARK BROADENED SPECTRA
+        """
+        mmm_coeff = {'6t2': {'C': 3.954E-16, 'a': 0.7149, 'b': 0.028}}
+
+        for diag_key in res_dict.keys():
+            for chord_key in res_dict[diag_key].keys():
+
+                print('Fitting Stark broadened H6-2 spectra, LOS id= :', diag_key, ' ', chord_key)
+
+                for H_line_key in res_dict[diag_key][chord_key]['spec_line_dict']['1']['1'].keys():
+
+                    if res_dict[diag_key][chord_key]['spec_line_dict']['1']['1'][H_line_key][0] == '6' and \
+                                    res_dict[diag_key][chord_key]['spec_line_dict']['1']['1'][H_line_key][1] == '2':
+
+                        wave_stark = np.asarray(res_dict[diag_key][chord_key]['los_int']['stark']['wave'])
+                        synth_data_stark = np.asarray(res_dict[diag_key][chord_key]['los_int']['stark']['intensity'])
+
+                        params = Parameters()
+                        params.add('cwl', value=float(H_line_key) / 10.0)
+                        params.add('area', value=float(
+                            res_dict[diag_key][chord_key]['los_int']['H_emiss'][H_line_key]['excit'] +
+                            res_dict[diag_key][chord_key]['los_int']['H_emiss'][H_line_key]['recom']))
+                        params.add('stark_fwhm', value=0.5, min=0.001, max=10.0)
+
+                        params['cwl'].vary = False
+                        params['area'].vary = False
+
+                        fit_result = minimize(self.residual_lorentz_52, params,
+                                              args=(wave_stark, synth_data_stark), method='leastsq')
+                        data_fit_ne = self.residual_lorentz_52(params, wave_stark, None, None)
+                        print("Chi squred: ", fit_result.chisqr)
+                        report_fit(params)
+
+                        # Assume Te = 1 eV
+                        fit_ne = np.power((params['stark_fwhm'].value / mmm_coeff['6t2']['C']),
+                                          1. / mmm_coeff['6t2']['a'])
+
+                        ##### Add fit ne result to dictionary
+                        res_dict[diag_key][chord_key]['los_int']['stark'] = {'fit': {'ne': fit_ne, 'units': 'm^-3'}}
+
+    def recover_line_int_particle_bal(self, res_dict):
+        """
+            ESTIMATE RECOMBINATION/IONISATION RATES USING ADF11 ACD, SCD COEFF
+        """
+        for diag_key in res_dict.keys():
+            for chord_key in res_dict[diag_key].keys():
+
+                if (res_dict[diag_key][chord_key]['los_int']['stark']['fit']['ne'] and
+                    res_dict[diag_key][chord_key]['los_int']['ff_fb_continuum']['fit']['fit_te_360_400']):
+
+                    fit_ne = res_dict[diag_key][chord_key]['los_int']['stark']['fit']['ne']
+                    fit_Te = res_dict[diag_key][chord_key]['los_int']['ff_fb_continuum']['fit']['fit_te_360_400']
+
+                    print('Ionization/recombination, LOS id= :', diag_key, ' ', chord_key)
+
+                    # area_cm2 = 2*pi*R*dW
+                    w2unmod = res_dict[diag_key][chord_key]['chord']['w2']
+                    # area_cm2 = 1.0e04 * 2.*np.pi*res_dict[diag_key][chord_key]['chord']['d2unmod']*res_dict[diag_key][chord_key]['coord']['v2'][0]
+                    area_cm2 = 1.0e04 * 2. * np.pi * w2unmod * \
+                               res_dict[diag_key][chord_key]['chord']['p2'][0]
+                    idxTe, Te_val = self.find_nearest(self.ADAS_dict['adf11']['1'].Te_arr, fit_Te)
+                    idxne, ne_val = self.find_nearest(self.ADAS_dict['adf11']['1'].ne_arr, fit_ne * 1.0E-06)
+                    for H_line_key in res_dict[diag_key][chord_key]['spec_line_dict']['1']['1'].keys():
+                        if res_dict[diag_key][chord_key]['spec_line_dict']['1']['1'][H_line_key][0] == '7' and \
+                                        res_dict[diag_key][chord_key]['spec_line_dict']['1']['1'][H_line_key][1] == '2':
+                            h72 = res_dict[diag_key][chord_key]['los_int']['H_emiss'][H_line_key]['excit'] + \
+                                  res_dict[diag_key][chord_key]['los_int']['H_emiss'][H_line_key]['recom']
+                            srec = 1.0E-04 * area_cm2 * h72 * 4. * np.pi * \
+                                   self.ADAS_dict['adf11']['1'].acd[idxTe, idxne] / \
+                                   self.ADAS_dict['adf15']['1']['1'][H_line_key + 'recom'].pec[idxTe, idxne]
+                        if res_dict[diag_key][chord_key]['spec_line_dict']['1']['1'][H_line_key][0] == '4' and \
+                                        res_dict[diag_key][chord_key]['spec_line_dict']['1']['1'][H_line_key][1] == '2':
+                            h42_excit = res_dict[diag_key][chord_key]['los_int']['H_emiss'][H_line_key]['excit']
+                            sion = 1.0E-04 * area_cm2 * h42_excit * 4. * np.pi * \
+                                   self.ADAS_dict['adf11']['1'].scd[idxTe, idxne] / \
+                                   self.ADAS_dict['adf15']['1']['1'][H_line_key + 'excit'].pec[idxTe, idxne]
+
+                    ##### Add adf11 srec sion estimates to dictionary
+                    res_dict[diag_key][chord_key]['los_int']['adf11_fit'] = {'Srec': srec, 'Sion': sion,
+                                                                  'units': 's^-1'}
+
+    def recover_delL_atomden_product(self, res_dict):
+        """
+            ESTIMATE DEL_L * ATOMIC DENSITY PRODUCT FROM LY-ALPHA ASSUMING EXCITATION DOMINATED
+        """
+        for diag_key in res_dict.keys():
+            for chord_key in res_dict[diag_key].keys():
+
+                if (res_dict[diag_key][chord_key]['los_int']['stark']['fit']['ne'] and
+                        res_dict[diag_key][chord_key]['los_int']['ff_fb_continuum']['fit']['fit_te_360_400']):
+
+                    fit_ne = res_dict[diag_key][chord_key]['los_int']['stark']['fit']['ne']
+                    fit_Te = res_dict[diag_key][chord_key]['los_int']['ff_fb_continuum']['fit']['fit_te_360_400']
+
+                    print('delL * n0 from Ly-alpha, LOS id= :', diag_key, ' ', chord_key)
+
+                    for H_line_key in res_dict[diag_key][chord_key]['spec_line_dict']['1']['1'].keys():
+                        if res_dict[diag_key][chord_key]['spec_line_dict']['1']['1'][H_line_key][0] == '2' and \
+                                        res_dict[diag_key][chord_key]['spec_line_dict']['1']['1'][H_line_key][1] == '1':
+                            h21 = res_dict[diag_key][chord_key]['los_int']['H_emiss'][H_line_key]['excit'] + \
+                                  res_dict[diag_key][chord_key]['los_int']['H_emiss'][H_line_key]['recom']
+                            idxTe, Te_val = self.find_nearest(
+                                self.ADAS_dict['adf15']['1']['1'][H_line_key + 'recom'].Te_arr, fit_Te)
+                            idxne, ne_val = self.find_nearest(
+                                self.ADAS_dict['adf15']['1']['1'][H_line_key + 'recom'].ne_arr, fit_ne * 1.0E-06)
+                            n0delL_Lya_tmp = 4. * np.pi * 1.0e-04 * h21 / (
+                                self.ADAS_dict['adf15']['1']['1'][H_line_key + 'excit'].pec[idxTe, idxne] * ne_val)
+                            n0delL_Lya_tmp = n0delL_Lya_tmp * 1.0e06 * 1.0e-02  # convert to m^-2
+                            ##### Add fit n0*delL result to dictionary
+                            res_dict[diag_key][chord_key]['los_int']['Ly_alpha_fit'] = {'n0delL': n0delL_Lya_tmp,
+                                                                             'units': 'm^-2'}
+
+
+if __name__=='__main__':
+
+    spec_line_dict = {
+        # Must include at least one spectral line from each impurity species in EDGE2D
+
+        '1': # HYDROGEN
+            {'1': {'1215.2': ['2', '1'],
+                   '1025.3': ['3', '1'],
+                   '6561.9': ['3', '2'],
+                   '4860.6': ['4', '2'],
+                   '4339.9': ['5', '2'],
+                   '4101.2': ['6', '2'],
+                   '3969.5': ['7', '2'],},
+             },
+
+        '4':  # BERYLLIUM
+            {'2':{'5272.32':['4s', '3p']}},
+
+        '7': # NITROGEN
+            {'2':{'4042.07':['4f', '3d'],
+                  '5002.18':['3d', '3p'],
+                  '5005.86':['3d', '3p']},
+             '3':{'4100.51':['3p', '3s']},
+             '4':{'3481.83':['3p', '3s']}
+             }
+    }
+
+    input_dict = {
+        'machine':'JET',
+        # 'tranfile': '/u/cstavrou/cmg/catalog/edge2d/jet/81472/may1117/seq#2/tran',
+        'tranfile':'/u/bloman/cmg/catalog/edge2d/jet/81472/sep1217/seq#3/tran',
+        # 'tranfile': '/u/cstavrou/cmg/catalog/edge2d/jet/81472/jun0617/seq#2/tran',
+        'diag_list': ['KT3A'],
+        # 'diag_list':['KT3A', 'KT1V'],
+        'spec_line_dict':spec_line_dict,
+        'interactive_plots':False,
+        'save_dir':'/work/bloman/pyproc/',
+        'run_options':{
+            'calc_synth_spec_features':True,
+            'analyse_synth_spec_features':True}
+    }
+
+    PyprocAnalyse(input_dict)
---
+++ pyprocplot.py
@@ -0,0 +1,197 @@
+
+# Import external modules
+
+import numpy as np
+import json, pprint, pickle
+import operator
+from functools import reduce
+import matplotlib.pyplot as plt
+from collections import namedtuple
+
+class PyprocPlot():
+    """
+        Class for retrieving, reducing and plotting pyproc saved data
+    """
+    def __init__(self, savedir, stdJETplots = False,
+                 plot_LOS_defs  = None,
+                 plot_profile_defs = None):
+        self.savedir = savedir
+        self.stdJETplots = stdJETplots
+        self.plot_LOS_defs = plot_LOS_defs
+        self.plot_profile_defs = plot_profile_defs
+
+        # Read pickled pyproc object
+        try:
+            with open(self.savedir + '/pyproc.2ddata.pkl', 'rb') as f:
+                self.__data2d = pickle.load(f)
+        except IOError as e:
+            raise
+
+        # Read processed synth diag saved data
+        try:
+            with open(self.savedir + '/pyproc.proc_synth_diag.json', 'r') as f:
+                self.__res_dict = json.load(f)
+        except IOError as e:
+            raise
+
+        if stdJETplots:
+            self.plotJET()
+
+    @property
+    def data2d(self):
+        return self.__data2d
+
+    @property
+    def res_dict(self):
+        return self.__res_dict
+
+    @staticmethod
+    def find_nearest(array, value):
+        idx = (np.abs(array - value)).argmin()
+        return idx, array[idx]
+
+    @staticmethod
+    def pprint_json(resdict, indent=0):
+        for key, value in resdict.items():
+            print('\t' * indent + str(key))
+            if isinstance(value, dict):
+                PyprocPlot.pprint_json(value, indent + 1)
+            else:
+                if isinstance(value, list):
+                    print('\t' * (indent+1) + '[list]')
+                else:
+                    if isinstance(value, str):
+                        print('\t' * (indent + 1) + value)
+                    else:
+                        print('\t' * (indent + 1) + '[float]')
+
+    # get item from nested dict
+    @staticmethod
+    def getFromDict(dataDict, mapList):
+        return reduce(operator.getitem, mapList, dataDict)
+
+    def getLineIntSortedDataByChordidx(self, diag, mapList):
+        """
+            input:
+                mapList: list of dict keys below the 'chord' level (e.g., ['los_int', 'stark', 'fit', 'ne']
+                diag: synth diag name string
+        """
+        tmp = []
+        chordidx = []
+        for chord in self.res_dict[diag]:
+            parval = PyprocPlot.getFromDict(self.res_dict[diag][chord], mapList)
+            # if isinstance(parval, float):
+            tmp.append(parval)
+            chordidx.append(int(chord)-1)
+
+        chords = np.asarray(chordidx)
+        sort_idx = np.argsort(chords, axis=0)
+        sorted_parvals = np.asarray(tmp)[sort_idx]
+
+        return sorted_parvals
+
+    # def get1DdataAtMaxLOSElecDen(self, diag, paramstr, nAvgNeighbs=2):
+    #
+    #     ne_los_1d_max_idx, val = find_nearest(res_dict[diag_key][chord_key]['los_1d']['ne'],
+    #                                           np.max(res_dict[diag_key][chord_key]['los_1d']['ne']))
+    #     # Find n0, Te corresponding to max ne along LOS (include nearest neighbours and average)
+    #     if (ne_los_1d_max_idx + 1) == len(res_dict[diag_key][chord_key]['los_1d']['n0']):
+    #         ne_los_1d_max.append(
+    #             np.average(np.array((res_dict[diag_key][chord_key]['los_1d']['ne'][ne_los_1d_max_idx - 1],
+    #                                  res_dict[diag_key][chord_key]['los_1d']['ne'][ne_los_1d_max_idx]))))
+    #
+    #     elif (ne_los_1d_max_idx + 2) == len(res_dict[diag_key][chord_key]['los_1d']['n0']):
+    #         ne_los_1d_max.append(
+    #             np.average(np.array((res_dict[diag_key][chord_key]['los_1d']['ne'][ne_los_1d_max_idx + 1],
+    #                                  res_dict[diag_key][chord_key]['los_1d']['ne'][ne_los_1d_max_idx]))))
+    #
+    #     elif (ne_los_1d_max_idx + 3) == len(res_dict[diag_key][chord_key]['los_1d']['n0']):
+    #         ne_los_1d_max.append(
+    #             np.average(np.array((res_dict[diag_key][chord_key]['los_1d']['ne'][ne_los_1d_max_idx + 2],
+    #                                  res_dict[diag_key][chord_key]['los_1d']['ne'][ne_los_1d_max_idx + 1],
+    #                                  res_dict[diag_key][chord_key]['los_1d']['ne'][ne_los_1d_max_idx]))))
+    #
+    #     else:
+    #         ne_los_1d_max.append(
+    #             np.average(np.array((res_dict[diag_key][chord_key]['los_1d']['ne'][ne_los_1d_max_idx + 3],
+    #                                  res_dict[diag_key][chord_key]['los_1d']['ne'][ne_los_1d_max_idx + 2],
+    #                                  res_dict[diag_key][chord_key]['los_1d']['ne'][ne_los_1d_max_idx + 1],
+    #                                  res_dict[diag_key][chord_key]['los_1d']['ne'][ne_los_1d_max_idx]))))
+    #
+    #
+    #     return sorted_parvals
+
+    # def plotLOS(self, include):
+
+    # SOME USEFUL PLOTTING CONFIGURATIONS
+
+    @staticmethod
+    def plotProf_ne_te_parbal_atden(x, ne, Te, Sion, Srec, n0, axobj=None, plot_at_max_ne_along_LOS=False,
+                                plot_target=False):
+        if axobj == None:
+            fig, axobj = plt.subplots(nrows=4, ncols=1, figsize=(6, 12), sharex=True)
+            left = 0.2  # the left side of the subplots of the figure
+            right = 0.95  # the right side of the subplots of the figure
+            bottom = 0.15  # the bottom of the subplots of the figure
+            top = 0.93  # the top of the subplots of the figure
+            wspace = 0.18  # the amount of width reserved for blank space between subplots
+            hspace = 0.1  # the amount of height reserved for white space between subplots
+            plt.subplots_adjust(left=left, bottom=bottom, right=right, top=top, wspace=wspace, hspace=hspace)
+
+        axobj[0].plot(x, ne)
+        axobj[1].plot(x, Te)
+        axobj[2].semilogy(x, Sion)
+        axobj[3].semilogy(x, n0)
+
+if __name__=='__main__':
+
+    plot_LOS_defs = namedtuple('plot_LOS_defs', 'diag', 'chord', 'llim')
+    plot_profile_defs = namedtuple('plot_profile_defs', 'diag', 'chord', 'llim')
+
+    plot_LOS_defs.diag = 'KT3A'
+    plot_LOS_defs.chord = '12'
+    plot_LOS_defs.llim = [5.3, 5.6]
+
+    workdir = '/work/bloman/pyproc/'
+    # Example
+    savedir = workdir + 'bloman_cmg_catalog_edge2d_jet_81472_sep0617_seq#1'
+    o = PyprocPlot(savedir)
+
+    # Print out results dictionary tree
+    PyprocPlot.pprint_json(o.res_dict['KT3A']['1']['los_int'])
+
+    ne_kt3a = o.getLineIntSortedDataByChordidx('KT3A', ['los_int', 'stark', 'fit', 'ne'])
+    Te_kt3a = o.getLineIntSortedDataByChordidx('KT3A', ['los_int', 'ff_fb_continuum', 'fit', 'fit_te_360_400'])
+    Sion = o.getLineIntSortedDataByChordidx('KT3A', ['los_int', 'adf11_fit', 'Sion'])
+    Srec = o.getLineIntSortedDataByChordidx('KT3A', ['los_int', 'adf11_fit', 'Srec'])
+    n0delL = o.getLineIntSortedDataByChordidx('KT3A', ['los_int', 'Ly_alpha_fit', 'n0delL'])
+    p2_kt3a = o.getLineIntSortedDataByChordidx('KT3A', ['chord', 'p2'])
+
+
+    fig1, ax1 = plt.subplots(nrows=4, ncols=1, figsize=(6,12), sharex=True)
+    left  = 0.2  # the left side of the subplots of the figure
+    right = 0.95    # the right side of the subplots of the figure
+    bottom = 0.15   # the bottom of the subplots of the figure
+    top = 0.93      # the top of the subplots of the figure
+    wspace = 0.18   # the amount of width reserved for blank space between subplots
+    hspace = 0.1  # the amount of height reserved for white space between subplots
+    plt.subplots_adjust(left=left, bottom=bottom, right=right, top=top, wspace=wspace, hspace=hspace)
+
+    o.plotProf_ne_te_parbal_atden(p2_kt3a[:,0], ne_kt3a, Te_kt3a, Sion, Srec, n0delL, axobj=ax1)
+
+    savedir = workdir + 'bloman_cmg_catalog_edge2d_jet_81472_sep1217_seq#1'
+    o = PyprocPlot(savedir)
+
+    # Print out results dictionary tree
+    PyprocPlot.pprint_json(o.res_dict['KT3A']['1']['los_int'])
+
+    ne_kt3a = o.getLineIntSortedDataByChordidx('KT3A', ['los_int', 'stark', 'fit', 'ne'])
+    Te_kt3a = o.getLineIntSortedDataByChordidx('KT3A', ['los_int', 'ff_fb_continuum', 'fit', 'fit_te_360_400'])
+    Sion = o.getLineIntSortedDataByChordidx('KT3A', ['los_int', 'adf11_fit', 'Sion'])
+    Srec = o.getLineIntSortedDataByChordidx('KT3A', ['los_int', 'adf11_fit', 'Srec'])
+    n0delL = o.getLineIntSortedDataByChordidx('KT3A', ['los_int', 'Ly_alpha_fit', 'n0delL'])
+    p2_kt3a = o.getLineIntSortedDataByChordidx('KT3A', ['chord', 'p2'])
+
+    o.plotProf_ne_te_parbal_atden(p2_kt3a[:,0], ne_kt3a, Te_kt3a, Sion, Srec, n0delL, axobj=ax1)
+
+    plt.show()
